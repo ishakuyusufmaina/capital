c108b92460cf0a8ec3bc6d807678d3b05520661c
Maina <75148285+ishakuyusufmaina@users.noreply.github.com>
2025-12-06 06:07:06 +0100

    Add files via upload

---
+++ resultConfig.js
@@ -0,0 +1,11 @@
+  const resultConfig = {
+    apiKey: "AIzaSyCWgh5RIrma5iVSnMNU4vedtQadReDDxqA",
+    authDomain: "result-32f33.firebaseapp.com",
+    projectId: "result-32f33",
+    storageBucket: "result-32f33.firebasestorage.app",
+    messagingSenderId: "518318428794",
+    appId: "1:518318428794:web:0e49851c783ea8c90d61c3",
+    measurementId: "G-N1EV4XNHNL"
+  };
+
+
