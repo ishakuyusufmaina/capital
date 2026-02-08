3a2db60ef77e4d4e6a99b0434145fd879f0db90c
Maina <75148285+ishakuyusufmaina@users.noreply.github.com>
2026-02-08 01:09:30 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const studentsConfig = {
+  apiKey: "AIzaSyDVM6rv3aGwSCq1jJtMAj0ge2s4frsbjHk",
+  authDomain: "capital-student.firebaseapp.com",
+  projectId: "capital-student",
+  storageBucket: "capital-student.firebasestorage.app",
+  messagingSenderId: "484457339321",
+  appId: "1:484457339321:web:efd77938776379d6a87e68",
+  measurementId: "G-X20R5GTYPG"
+};
+
